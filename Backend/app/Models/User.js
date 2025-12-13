5f2291a46fcf8e45420f51df25cc0a5cc3db85c8
abdellah <abdo.abdell.2000@gmail.com>
2025-12-13 15:50:24 +0100

    faire des ameliorations pour inserer un ensemble de data pour user et ameliorer la methode update du track et trailer

--- Backend/app/Models/User.js
+++ Backend/app/Models/User.js
@@ -55,7 +55,7 @@ userSchema.pre('save', async function () {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
     } catch (error) {
-        return new Error(error);
+        throw error;
     }
 });
 
--- Backend/app/Repositories/TruckRepository.js
+++ Backend/app/Repositories/TruckRepository.js
@@ -4,6 +4,15 @@ class TruckRepository extends BaseRepository {
     constructor(TruckModel) {
         super(TruckModel);
     }
+
+    async update(id, data) {
+        const doc = await this.model.findById(id);
+        if (!doc) {
+            return null;
+        }
+        doc.set(data);
+        return await doc.save();
+    }
 }
 
 module.exports = TruckRepository;
